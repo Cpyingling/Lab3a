2636434ea24fee6773e466fe15f6d08f664d8f2d
Cpyingling <89798338+Cpyingling@users.noreply.github.com>
2021-09-15 01:54:46 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+/*Author Charles Yingling       Date 9/14/2021
+1. Write a program in C# to convert Celsius to Farehneit. 
+
+If temperature is below freezing, display "This temperature is below freezing of 32 degree Farenheit"
+If temperature is above boiling, display "This temperature is above boiling point of 212 degree Farenheit"
+
+*/
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //ask for temp in Celsius
+    Console.WriteLine ("Enter temperature in Celsius");
+    double cel = Convert.ToDouble(Console.ReadLine());
+    //convert Celsius to Farehneit
+    double far = (cel * 9/5) + 32;
+    //display Farehneit
+    Console.WriteLine ($"Farenheit equivalent for {cel} degree Celsius is {far}");
+    if (far < 32) {
+    Console.WriteLine ("This temperature is below freezing of 32 degrees Farenheit");
+    } 
+   if (far > 212) {
+    Console.WriteLine ("This temperature is above boiling point of 212 degrees Farenheit");
+    } 
+  }
+}
